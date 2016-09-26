295fa5c4121cf7be9cd43dea9ac7fc16dff9b667
brunobruno <brngalati@gmail.com>
2016-09-26 09:04:08 -0300

    change

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var pageLimit=140;
   $(document).ready(function() {
     for(var i = 1; i <= pageLimit; i++) {
-    $('body').append('<div class="class-'+i+'"></div>')
+    $('#board').append('<li class="class-'+i+'"></li>')
   }
 });
