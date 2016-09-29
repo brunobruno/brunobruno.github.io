4d51b15b5b3fd7711d220d51b894a659759040d5
brunobruno <brngalati@gmail.com>
2016-09-29 15:20:27 -0300

    upt main.js

--- main.js
+++ main.js
@@ -1,4 +1,32 @@
 var pageLimit=140;
+for( i=0; i < 1000; i++) {
+
+  var NumberRandom = Math.floor((Math.random() * 250) + 1); 
+  var NumberRandomforRotate = Math.floor((Math.random() * 360) + 1); 
+  var value = Math.random() * 0xFF | 0;
+  var grayscale = (value << 16) | (value << 8) | value;
+  var ColorRandom  = '#' + grayscale.toString(16);
+  var div = document.createElement('div');
+  var chars = "abcdefghijklmnopqurstuvwxyzABCDEFGHIJKLMNOPQURSTUVWXYZ";
+  var randomChars = chars.substr( Math.floor(Math.random() * 30), 1);
+  
+  div.innerHTML = randomChars;
+  div.style.height = 45 + 'px';
+  div.style.width = 45 + 'px';
+  div.style.fontSize = NumberRandom + 'px';
+  div.style.float = 'left';
+  div.style.webkitTransform = 'rotate('+NumberRandomforRotate+'deg)'; 
+  div.style.mozTransform    = 'rotate('+NumberRandomforRotate+'deg)'; 
+  div.style.msTransform     = 'rotate('+NumberRandomforRotate+'deg)'; 
+  div.style.oTransform      = 'rotate('+NumberRandomforRotate+'deg)'; 
+  div.style.transform       = 'rotate('+NumberRandomforRotate+'deg)'; 
+ // div.style.background = ColorRandom;
+ // div.style.color = ColorRandomText;
+  document.body.appendChild(div);
+}
+
+
+
   $(document).ready(function() {
     for(var i = 1; i <= pageLimit; i++) {
     $('#board').append('<li class="class-'+i+'"></li>')
