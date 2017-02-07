33a22384b6795facd073b944d333d7534613fd44
brunobruno <brngalati@gmail.com>
2017-02-07 08:48:11 -0200

    up

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-var pageLimit=240;
+var pageLimit=140;
+var pageLimitVortex:540;
 
 
 function montaLetras() {
@@ -11,7 +12,7 @@ function montaLetras() {
       var grayscale = (value << 16) | (value << 8) | value;
       var ColorRandom  = '#' + grayscale.toString(16);
       var div = document.createElement('div');
-      var chars = "abcdefghijklmnopqurstuvwxyzABCDEFGHIJKLMNOPQURSTUVWXYZ";
+      var chars = "abcdefghijklmnopqurstuvwxyzABCDEFGHIJKLMNOPQURSTUVWXYZ1234567890";
       var randomChars = chars.substr( Math.floor(Math.random() * 30), 1);
 
       div.innerHTML = randomChars;
@@ -37,8 +38,11 @@ $(document).ready(function() {
     for(var i = 1; i <= pageLimit; i++) {
       
     $('#board').append('<li class="class-'+i+'"></li>');
-    $('body').append('<div class="item" style="left:'+i+'px; top:'+i+'px; transform: rotate('+i+'deg);width:1'+i+'px;"></div>')
+    )
   
     }
+  for(var i = 1; i <= pageLimitVortex; i++) {
+    $('body').append('<div class="item" style="left:'+i+'px; top:'+i+'px; transform: rotate('+i+'deg);width:1'+i+'px;"></div>'
+  }
   
 });
