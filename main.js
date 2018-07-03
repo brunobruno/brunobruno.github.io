d7c7f0a14800b10a49d99423aaa887838de25715
brunobruno <brngalati@gmail.com>
2018-07-02 22:35:58 -0300

    trash

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-// just a experiment, the important here is that i accomplished what was on my head
-
-var pageLimit=140;
-var pageLimitVortex=240;
+var pageLimit=240;
+var pageLimitVortex=540;
 
 
 function montaLetras() {
+  $('#letters').empty();
    for( i=0; i < 1000; i++) {
 
       var NumberRandom = Math.floor((Math.random() * 250) + 1); 
@@ -14,7 +13,6 @@ function montaLetras() {
       var ColorRandom  = '#' + grayscale.toString(16);
       var div = document.createElement('div');
       var body = document.getElementsByTagName('body');
-      var letters - document.getElementById('letters');
       var chars = "abcdefghijklmnopqurstuvwxyzABCDEFGHIJKLMNOPQURSTUVWXYZ1234567890";
       var randomChars = chars.substr( Math.floor(Math.random() * 30), 1);
 
@@ -32,8 +30,7 @@ function montaLetras() {
       div.style.transform       = 'rotate('+NumberRandomforRotate+'deg)';
       document.body.style.backgroundColor = ColorRandom;
      
-      letters.appendChild(div);
-      div.appendChild(letters);
+      $('#letters').append(div);
 
   }
 }
@@ -50,15 +47,15 @@ function montaLetras() {
 
 
 
-// $(document).ready(function() {
-//     montaLetras();
-//     for(var i = 1; i <= pageLimit; i++) { 
-//       $('#board').append('<li class="class-'+i+'"></li>');
-//     }
-//   for(var i = 1; i <= pageLimitVortex; i++) {
-//     var NumberRandomVortex = Math.floor((Math.random() * 250) + 1); 
-// //  $('body').append('<div class="item" style="left:'+i+'px; top:'+i+'px; transform: rotate('+i+'deg);width:1'+i+'px;"></div>');
-//     $('body').append('<div class="item" style="left:0.'+i+'px; top:'+i+'px; transform: rotate3d(1,1,1,'+i+'deg);width:1'+i+'px;"></div>');
-//   }
+$(document).ready(function() {
+    montaLetras();
+    for(var i = 1; i <= pageLimit; i++) { 
+      $('#board').append('<li class="class-'+i+'"></li>');
+    }
+  for(var i = 1; i <= pageLimitVortex; i++) {
+    var NumberRandomVortex = Math.floor((Math.random() * 250) + 1); 
+//  $('body').append('<div class="item" style="left:'+i+'px; top:'+i+'px; transform: rotate('+i+'deg);width:1'+i+'px;"></div>');
+    $('body').append('<div class="item" style="left:0.'+i+'px; top:'+i+'px; transform: rotate3d(1,1,1,'+i+'deg);width:1'+i+'px;"></div>');
+  }
 
 });
